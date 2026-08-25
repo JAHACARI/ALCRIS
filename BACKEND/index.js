b223185c28f1583b2fd5795d5cba6568c9b07403
JAHACARI <janiersena25@gmail.com>
2026-08-24 19:37:29 -0500

    se ha hecho la seccion de seguimiento  para las 3 carpetas, controllers, models, auth y se modifico el index

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -7,6 +7,7 @@ import cors from "cors";
 import routerAuth from "./routes/authRoute.js";
 import routerUser from "./routes/userRoute.js";
 import routerReserva from "./routes/reservaRoute.js";
+import routerSeguimiento from "./routes/seguimientoRoute.js";
 import { conectaDB } from "./config/supabase.js";
 
 const app = express();
@@ -28,6 +29,7 @@ conectaDB();
 app.use("/auth", routerAuth);
 app.use("/usuarios", routerUser);
 app.use("/reservas", routerReserva);
+app.use("/seguimiento", routerSeguimiento);
 
 //----------------------------------------------------------//
 // Ruta inicial
