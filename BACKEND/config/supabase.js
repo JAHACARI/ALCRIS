74c7b4d316fb355f0845c57844287f680fe56190
felipe <johanfelipecastillo8@gmail.com>
2026-08-25 09:50:54 -0500

    arreglo comentarios

--- BACKEND/config/supabase.js
+++ BACKEND/config/supabase.js
@@ -1,18 +1,17 @@
-//----------------------------------------------------------//
+
 // Configuración de Supabase
-//----------------------------------------------------------//
+
 import "dotenv/config";
 import { createClient } from "@supabase/supabase-js";
 
-//----------------------------------------------------------//
+
 // Variables de entorno
-//----------------------------------------------------------//
+
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_KEY;
 
-//----------------------------------------------------------//
 // Validación de variables de entorno
-//----------------------------------------------------------//
+
 if (!supabaseUrl || !supabaseKey) {
   console.error(
     "❌ Error: las variables de entorno SUPABASE_KEY y SUPABASE_URL son requeridas.",
