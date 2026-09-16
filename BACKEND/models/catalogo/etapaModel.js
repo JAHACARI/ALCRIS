afb6b1529105ee24de86ad6ca238ab3e5704e4c9
JANIER_HAIR <janiersena25@gmail.com>
2026-09-16 07:46:53 -0500

    Delete BACKEND directory

--- BACKEND/models/catalogo/etapaModel.js
+++
@@ -1,9 +0,0 @@
-import { supabase } from "../../config/supabase.js";
-
-export const obtenerEtapas = async () => {
-  const { data, error } = await supabase
-    .from("etapas_proceso")
-    .select("*")
-    .order("orden");
-  return { data, error };
-};
